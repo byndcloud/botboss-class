743746d5bd957f68860b86a54d96eeab7585ae6c
José Ricardo Alves Figueirôa <josericardoaf@gmail.com>
2023-07-19 10:11:37 -0300

    feature: puppeteer demo

---
+++ pptr-demo/index.js
@@ -0,0 +1,20 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto('https://developer.chrome.com/');
+
+  // Set screen size
+  await page.setViewport({width: 1080, height: 1024});
+
+  // Type into search box
+//   await page.type('.search-box__input', 'automate beyond recorder');
+await page.click('.project-card')
+
+})();
