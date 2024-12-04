01723b538c394ce61b49a52fe1aeec2a00b23bcb
José Ricardo Alves Figueirôa <josericardoaf@gmail.com>
2024-12-04 11:56:57 -0300

    feature: refactor country data extraction to use Puppeteer for web scraping

--- paises/index.js
+++ paises/index.js
@@ -1,57 +1,33 @@
-
-/**
- * Extrai e retorna o texto de um elemento HTML com base em um seletor CSS.
- *
- * @param {Element} element - O elemento HTML de onde o texto será extraído.
- * @param {string} selector - O seletor CSS usado para encontrar o elemento desejado.
- * @returns {string} O texto extraído do elemento encontrado, ou undefined se o elemento não for encontrado.
- *
- * @example
- * // '<div><span class="country">Brasil</span></div>';
- * const country = parseCountryElement(element, '.country');
- * console.log(country); // "Brasil"
- */
-function parseCountryElement(element, selector) {
-    return element.querySelector(selector)?.textContent.trim();
-}
-
-
-/**
- * Extrai dados do país a partir de um elemento HTML.
- *
- * @param {HTMLElement} element - O elemento HTML que contém os dados do país.
- * @returns {Object} Um objeto contendo os dados do país.
- * @returns {string} name - O nome do país.
- * @returns {string} capital - A capital do país.
- * @returns {string} population - A população do país.
- * @returns {string} area - A área do país.
- */
-function extractCountryData(element) {
-  const name = parseCountryElement(element, '.country-name')
-  const capital = parseCountryElement(element, '.country-capital')
-  const population = parseCountryElement(element, '.country-population')
-  const area = parseCountryElement(element, '.country-area')
-
-  return {
-    name,
-    capital,
-    population,
-    area,
-  };
-}
+import puppeteer from 'puppeteer'
 
 async function main() {
     const url = 'https://www.scrapethissite.com/pages/simple/'
     const page = await setupPage(url)
     await page.waitForSelector('section#countries') // espera que os países sejam carregados
-    const countries = await page.$$eval('div.country', elements => elements.map(extractCountryData))
+
+    const countries = await page.$$eval('.country', (elements) => {
+        return elements.map((el) => {
+        const name = el.querySelector('.country-name')?.textContent.trim();
+        const capital = el.querySelector('.country-capital')?.textContent.trim();
+        const population = el.querySelector('.country-population')?.textContent.trim();
+        const area = el.querySelector('.country-area')?.textContent.trim();
+
+        return {
+            name,
+            capital,
+            population,
+            area,
+        };
+        });
+    });
+
     console.table(countries)
 }
 
 main().then()
 
 async function setupPage(url) {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({ headless: false })
     const page = await browser.newPage()
     await page.goto(url)
     return page
